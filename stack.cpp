76884fa55e8342cb668737af98c99e77a8e698d2
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-05 16:52:19 +0700

    update the top pointer to the new node

--- stack.cpp
+++ stack.cpp
@@ -33,3 +33,4 @@ public:
         Node *newNode = new Node(); // 1. Allocate memory for the new node
         newNode->data = value;      // 2. Assign value
         newNode->next = top;        // 3. Set the next pointer of the new node to the current
+        top = newNode;              // 4. Update the top pointer to the new node
