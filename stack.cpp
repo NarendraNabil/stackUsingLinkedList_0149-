d9ca9785c80d8e60cee8cf8bc8d012834e667652
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-05 16:45:28 +0700

    initialize the stack with a null top pointer

--- stack.cpp
+++ stack.cpp
@@ -21,3 +21,9 @@ class Stack
 private:
     Node *top; // pointer to the top node of the stack
 
+public: 
+    Stack()
+    {
+        top = NULL; // initialize the stack with a null top pointer
+    }
+
